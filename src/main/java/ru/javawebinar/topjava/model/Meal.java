da27818da72df30a8e30e342d3bdde77f0d15eb2
sbn <zgreen3@gmail.com>
2021-06-29 23:37:18 +0300

    H/W 4 for Lesson 4 [topjava-new-1-06-21]

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,17 +1,56 @@
 package ru.javawebinar.topjava.model;
 
+import org.springframework.lang.NonNull;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-public class Meal {
-    private final LocalDateTime dateTime;
+@NamedQueries({
+        @NamedQuery(name = Meal.BY_USER_ID, query = "SELECT m FROM Meal m WHERE m.id=:id AND m.user.id=:user_id"),
+        @NamedQuery(name = Meal.ALL_SORTED, query = "SELECT m FROM Meal m WHERE m.user.id=:user_id ORDER BY m.dateTime, m.calories"),
+        @NamedQuery(name = Meal.BETWEEN_HALF_OPEN_SORTED, query = "SELECT m FROM Meal m WHERE m.dateTime>=:start_datetime " +
+                "AND m.dateTime<=:end_datetime AND m.user.id=:user_id ORDER BY m.dateTime, m.calories"),
+        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:user_id"),
+})
+@Entity
+@Table(name = "meals", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "date_time"}, name = "meals_unique_user_datetime_idx")})
+public class Meal extends AbstractBaseEntity {
+    public static final String BY_USER_ID = "Meal.getByUserId";
+    public static final String ALL_SORTED = "Meal.getAllSorted";
+    public static final String BETWEEN_HALF_OPEN_SORTED = "Meal.getBetweenHalfOpenSorted";
+    public static final String DELETE = "Meal.delete";
 
-    private final String description;
+    @Column(name = "date_time", nullable = false)
+    @NotNull
+    private LocalDateTime dateTime;
 
-    private final int calories;
+    @Column(name = "description", nullable = false)
+    @NotNull
+    private String description;
+
+    @Column(name = "calories", nullable = false)
+    @NotNull
+    private int calories;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn
+    //@NotNull
+    @NotEmpty
+    private User user;
+
+    public Meal() {
+    }
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
+        this(null, dateTime, description, calories);
+    }
+
+    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
+        super(id);
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
@@ -35,5 +74,35 @@ public class Meal {
 
     public LocalTime getTime() {
         return dateTime.toLocalTime();
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Meal{" +
+                "id=" + id +
+                ", dateTime=" + dateTime +
+                ", description='" + description + '\'' +
+                ", calories=" + calories +
+                '}';
     }
 }
